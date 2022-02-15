132cd6fd46a5e99b2dea19959eca62a5b40b0169
sarahherrmann <83958698+sarahherrmann@users.noreply.github.com>
2022-02-15 15:04:31 +0100

    added histograms

--- PWGMM/Tasks/analyse-mft-jets.cxx
+++ PWGMM/Tasks/analyse-mft-jets.cxx
@@ -45,6 +45,12 @@ struct analyseMFTJets {
     "registry",
     {
       {"TracksPhiEta", "; #varphi; #eta; tracks", {HistType::kTH2F, {{600, -M_PI, M_PI}, {35, -4.5, -1.}}}}, //
+      {"NTracksOverNparts", "Number of reconstructed tracks / number of particles in a collision; Ntracks/NParts; #count", {HistType::kTH1F, {{300, 0, 150}}}}, //
+      {"NTracksPerJet", "Number of reconstructed tracks per jet; Ntracks/jet; #count", {HistType::kTH1F, {{30, 1, 30}}}}, //
+      {"NPartsPerJet", "Number of particles per jet; Nparts/jet; #count", {HistType::kTH1F, {{30, 1, 30}}}}, //
+      {"AreaMCjet", "area of jet in gen MC; area; #count", {HistType::kTH1F, {{300, 0, 30}}}}, //
+      {"AreaRecojet", "area of jet in reco MC; area; #count", {HistType::kTH1F, {{300, 0, 30}}}}, //
+
 
     }                                                                                //
   };
@@ -75,6 +81,9 @@ struct analyseMFTJets {
     TLorentzVector vPart;
     std::vector<PseudoJet> particlesGen;
 
+    float Ntracks = 0;
+    float Nparts = 0;
+
     //int nChargedPrimaryParticles = 0;
     //auto z = mcCollision.posZ();
 
@@ -84,13 +93,14 @@ struct analyseMFTJets {
     {
       printf("collision index : %d\n", collision.globalIndex());
       auto groupedTracks = tracks.sliceBy(o2::aod::fwdtrack::collisionId, collision.globalIndex());
+      Ntracks = groupedTracks.size();
       for (auto& track : groupedTracks)
       {
         float phi = track.phi();
         o2::math_utils::bringTo02Pi(phi);
         vTrack.setPtEtaPhiM(0.2,track.eta(),phi,0.138);
         particlesRec.push_back(PseudoJet(vTrack.Px(), vTrack.Py(), vTrack.Pz(), vTrack.E()));
-        particlesRec[particlesRec.size()-1].user_index(track.mcParticleId());
+        particlesRec[particlesRec.size()-1].set_user_index(track.mcParticleId());//set_user_index
 
       }
 
@@ -103,9 +113,12 @@ struct analyseMFTJets {
       {
         printf("jet %d : pt %f y %f phi %f\n", i, jetsRec[i].pt(), jetsRec[i].rap(), jetsRec[i].phi());
         vector<PseudoJet> constituents = jetsRec[i].constituents();
+
+        registry.fill(HIST("AreaRecojet"), jetsRec[i].area());
+        registry.fill(HIST("NTracksPerJet"), constituents.size());
         for (unsigned j = 0; j < constituents.size(); j++)
         {
-          printf("constituent %d : pt %f y %f phi %f\n", j, constituents[j].pt(), constituents[j].rap(), constituents[j].phi());
+          printf("constituent %d : pt %f, y %f, phi %f, index %d\n", j, constituents[j].pt(), constituents[j].rap(), constituents[j].phi(), constituents[j].user_index());
         }
       }
     }
@@ -121,11 +134,13 @@ struct analyseMFTJets {
       {
         charge = p->Charge();
       }
-      if (charge != 0 && particle.isPhysicalPrimary())
+      if (charge != 0 && particle.isPhysicalPrimary() && (particle.eta() < -2.3 && particle.eta() > -3.6))
+      //charged primary and within the MFT acceptance
       {
+        Nparts++;
         vPart.setPtEtaPhiM(particle.pt(),particle.eta(),particle.phi(),p->Mass());
         particlesGen.push_back(PseudoJet(vPart.Px(), vPart.Py(), vPart.Pz(), vPart.E()));
-        particlesGen[particlesGen.size()-1].user_index(particle.globalIndex());
+        particlesGen[particlesGen.size()-1].set_user_index(particle.globalIndex());
       }
     }
 
@@ -133,6 +148,23 @@ struct analyseMFTJets {
     fastjet::ClusterSequenceArea csGen(particlesGen, jet_def, areaDef);
 
     jetsGen = csGen.inclusive_jets(0.0);
+
+    for (unsigned i = 0; i < jetsGen.size(); i++)
+    {
+      printf("jet %d : pt %f y %f phi %f\n", i, jetsGen[i].pt(), jetsGen[i].rap(), jetsGen[i].phi());
+      vector<PseudoJet> constituents = jetsGen[i].constituents();
+
+      registry.fill(HIST("AreaMCjet"), jetsGen[i].area());
+      registry.fill(HIST("NPartsPerJet"), constituents.size());
+      for (unsigned j = 0; j < constituents.size(); j++)
+      {
+        printf("constituent %d : pt %f, y %f, phi %f, index %d\n", j, constituents[j].pt(), constituents[j].rap(), constituents[j].phi(), constituents[j].user_index());
+        //particles[mother0Id]
+      }
+    }
+
+
+    registry.fill(HIST("NTracksOverNparts"), Ntracks/Nparts);
   }
 
   PROCESS_SWITCH(analyseMFTJets, processGen, "Process gen level", true);
